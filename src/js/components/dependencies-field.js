930fa9721b8f5f348e04f0edf8fcb910e47871b8
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-05-01 08:52:41 +0100

    Init demo data

--- src/js/components/dependencies-field.js
+++ src/js/components/dependencies-field.js
@@ -67,8 +67,57 @@ var DependenciesField = React.createClass({
 
   },
 
+  generateDemoData: function () {
+    var data = {
+      "name": "dep-compare",
+      "version": "0.0.1",
+      "description": "Comparing NPM (dev)Dependencies",
+      "repository": {
+        "type": "git",
+        "url": "https://github.com/ekonstantinidis/git-compare.git"
+      },
+
+      "dependencies": {
+        "bootstrap":"^3.3.4",
+        "browserify":"^9.0.8",
+        "font-awesome":"^4.3.0",
+        "react":"^0.13.2",
+        "react-bootstrap":"^0.21.0",
+        "react-tools":"^0.13.2",
+        "reactify":"^1.1.0",
+        "reflux":"^0.2.7",
+        "superagent":"^1.2.0",
+        "watchify":"^3.1.2"
+      },
+
+      "devDependencies": {
+        "grunt":"^0.4.5",
+        "grunt-contrib-clean":"^0.6.0",
+        "grunt-contrib-copy":"^0.8.0",
+        "grunt-contrib-less":"^1.0.1",
+        "grunt-contrib-watch":"^0.6.1",
+        "jshint-stylish":"^1.0.1",
+        "jsxhint":"=0.14.0",
+        "less":"=2.5.0"
+      }
+    };
+
+    for (var key in data) {
+
+      if (key === 'dependencies' || key === 'devDependencies') {
+        var dependencies = data[key];
+
+        for (var name in dependencies) {
+          Actions.getDependency(key, name, dependencies[name]);
+        }
+      }
+
+    }
+
+  },
+
   gotDependenciesErrors: function () {
-    console.log("ERROR...");
+    console.log('ERROR...');
   },
 
   render: function () {
@@ -82,12 +131,17 @@ var DependenciesField = React.createClass({
               className='input-lg'
               bsStyle={this.validateInput()}
               hasFeedback
-              rows="12"
+              rows='12'
               label='Your package.json'
               placeholder='Enter dependencies'
               onChange={this.handleJsonChange} />
 
           </Col>
+
+          <Col mdOffset={3} md={6}>
+            <Button bsSize='large' onClick={this.generateDemoData}>Demo</Button>
+          </Col>
+
         </Row>
       </div>
     );
