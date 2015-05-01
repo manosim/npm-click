397f98c73f429a800eb16b44f67c028030589c46
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-05-01 19:37:24 +0100

    Compare versions in the store

--- src/js/components/package.js
+++ src/js/components/package.js
@@ -18,43 +18,8 @@ var Package = React.createClass({
     });
   },
 
-  compareVersionNumbers: function (v1, v2) {
-    // http://maymay.net/blog/2008/06/15/ridiculously-simple-javascript-version-string-to-object-parser/
-    function parseVersionString (str) {
-      if (typeof(str) != 'string') { return false; }
-      var x = str.split('.');
-      // parse from string or default to 0 if can't parse
-      var maj = parseInt(x[0]) || 0;
-      var min = parseInt(x[1]) || 0;
-      var pat = parseInt(x[2]) || 0;
-      return {
-        major: maj,
-        minor: min,
-        patch: pat
-      };
-    }
-
-    var running_version = parseVersionString(v1.replace(/[^0-9.]/g, ''));
-    var latest_version = parseVersionString(v2);
-    if (running_version.major < latest_version.major) {
-        // A major new update is available!
-        console.log('A major new update is available!');
-        return -1;
-    } else if (running_version.minor < latest_version.minor || running_version.patch < latest_version.patch) {
-        // A new minor or patch update is available.
-        console.log('A new minor or patch update is available.');
-        return 0;
-    } else {
-        // We are running the latest version! No need to update.
-        console.log('We are running the latest version! No need to update.');
-        return 1;
-    }
-  },
-
   upToDate: function () {
-    var installedVersion = this.state.dependency.version;
-    var latestVersion = this.state.dependency.current['dist-tags'].latest;
-    var isUpToDate = this.compareVersionNumbers(installedVersion, latestVersion);
+    var isUpToDate = this.state.dependency.status;
     if (isUpToDate === 1) {
       return 'has-latest fa fa-check-circle';
     } else if (isUpToDate === 0) {
--- src/js/stores/dependencies-store.js
+++ src/js/stores/dependencies-store.js
@@ -10,13 +10,47 @@ var DependenciesStore = Reflux.createStore({
     this._devDependencies = [];
   },
 
+  compareVersionNumbers: function (v1, v2) {
+    // http://maymay.net/blog/2008/06/15/ridiculously-simple-javascript-version-string-to-object-parser/
+    function parseVersionString (str) {
+      if (typeof(str) != 'string') { return false; }
+      var x = str.split('.');
+      // parse from string or default to 0 if can't parse
+      var maj = parseInt(x[0]) || 0;
+      var min = parseInt(x[1]) || 0;
+      var pat = parseInt(x[2]) || 0;
+      return {
+        major: maj,
+        minor: min,
+        patch: pat
+      };
+    }
+
+    var running_version = parseVersionString(v1.replace(/[^0-9.]/g, ''));
+    var latest_version = parseVersionString(v2);
+    if (running_version.major < latest_version.major) {
+        // A major new update is available!
+        return -1;
+    } else if (running_version.minor < latest_version.minor || running_version.patch < latest_version.patch) {
+        // A new minor or patch update is available.
+        return 0;
+    } else {
+        // We are running the latest version! No need to update.
+        return 1;
+    }
+  },
+
   makeRequest: function (type, name, version) {
+    var self = this;
+
     apiRequests
       .get('https://registry.npmjs.org/' + name)
       .end(function (err, response) {
         if (response.ok) {
           // Success - Do Something.
-          Actions.getDependency.completed(type, name, version, response.body);
+          var latestVersion = response.body['dist-tags'].latest;
+          var status = self.compareVersionNumbers(version, latestVersion);
+          Actions.getDependency.completed(type, name, version, status, response.body);
         } else {
           // Error - Show messages.
           Actions.getDependency.failed(response.body);
@@ -38,20 +72,20 @@ var DependenciesStore = Reflux.createStore({
       }
   },
 
-  onGetDependencyCompleted: function (type, name, version, response) {
-
-    console.log('Get Dependency - Success.');
+  onGetDependencyCompleted: function (type, name, version, status, response) {
 
     if (type === 'dependencies') {
       this._dependencies.push({
         'name': name,
         'version': version,
+        'status': status,
         'current': response
       });
     } else if (type === 'devDependencies') {
       this._devDependencies.push({
         'name': name,
         'version': version,
+        'status': status,
         'current': response
       });
     }
@@ -64,7 +98,6 @@ var DependenciesStore = Reflux.createStore({
   },
 
   onGetDependencyFailed: function (errors) {
-    console.log('Get Dependencies - Error.');
     Actions.onGetDependenciesErrors(errors);
   },
 
