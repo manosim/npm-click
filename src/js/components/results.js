24b862b1e41ded031026160e7b94bc9f80d8a4dd
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-05-02 01:32:11 +0100

    Fix key issue, prepare project details

--- src/js/components/results.js
+++ src/js/components/results.js
@@ -17,6 +17,8 @@ var Results = React.createClass({
   getInitialState: function () {
     return {
       project: {
+        name: undefined,
+        description: undefined,
         dependencies: [],
         devDependencies: []
       },
@@ -27,14 +29,20 @@ var Results = React.createClass({
   render: function () {
     return (
       <Grid className='results'>
+        <h2>Project Details</h2>
+        <Row>
+          <Col md={4}><small>name</small> {this.state.project.name}</Col>
+          <Col md={8}><small>description</small> {this.state.project.description}</Col>
+        </Row>
+
         <h2>Dependencies - {this.state.project.dependencies.length}</h2>
         {this.state.project.dependencies.map(function(object, i){
-          return <Package dependency={object} />;
+          return <Package key={object.name} dependency={object} />;
         })}
 
         <h2>DevDependencies - {this.state.project.devDependencies.length}</h2>
         {this.state.project.devDependencies.map(function(object, i){
-          return <Package dependency={object} />;
+          return <Package key={object.name} dependency={object} />;
         })}
       </Grid>
     );
