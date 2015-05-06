72a0ef5be50551459e1c778dea5079abfe3c1851
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-05-06 22:36:35 +0100

    New key

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,8 +52,8 @@ module.exports = function(grunt) {
             name: 'Emmanouil Konstantinidis',
             email: 'manos@iamemmanouil.com'
           },
-          repo: 'https://' + process.env.TRAVIS_SECURE_ENV_VARS + '@github.com/ekonstantinidis/npm-check.git',
-          message: 'Publish project to Github Pages (Auto)',
+          repo: 'https://' + process.env.GH_TOKEN + '@github.com/ekonstantinidis/npm-check.git',
+          message: 'Publish project to Github Pages (Auto)' + getDeployMessage(),
           silent: false
         },
         src: ['**/*']
@@ -61,6 +61,20 @@ module.exports = function(grunt) {
     }
 
   });
+
+  function getDeployMessage() {
+    var ret = '\n\n';
+    if (process.env.TRAVIS !== 'true') {
+      ret += 'missing env vars for travis-ci';
+      return ret;
+    }
+    ret += 'branch:       ' + process.env.TRAVIS_BRANCH + '\n';
+    ret += 'SHA:          ' + process.env.TRAVIS_COMMIT + '\n';
+    ret += 'range SHA:    ' + process.env.TRAVIS_COMMIT_RANGE + '\n';
+    ret += 'build id:     ' + process.env.TRAVIS_BUILD_ID  + '\n';
+    ret += 'build number: ' + process.env.TRAVIS_BUILD_NUMBER + '\n';
+    return ret;
+  }
 
   grunt.registerTask('check-deploy', function() {
     // need this
