126d7e739d248671fd2675b8c44712e67eda2531
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-05-06 23:38:39 +0100

    Update react-bootstrap

--- src/js/components/navigation.js
+++ src/js/components/navigation.js
@@ -1,17 +1,22 @@
 var React = require('react');
-var ReactBootstrap  = require('react-bootstrap');
+var ReactBootstrap = require('react-bootstrap');
+
+var Navbar = ReactBootstrap.Navbar;
+var Nav = ReactBootstrap.Nav;
+var NavItem = ReactBootstrap.NavItem;
+var CollapsibleNav = ReactBootstrap.CollapsibleNav;
 
 var Navigation = React.createClass({
   render: function () {
     return (
 
-      <ReactBootstrap.Navbar brand='Npm Check' inverse toggleNavKey={0}>
-        <ReactBootstrap.CollapsableNav eventKey={0}> {/* This is the eventKey referenced */}
-          <ReactBootstrap.Nav navbar right>
-            <ReactBootstrap.NavItem eventKey={1} href='https://github.com/ekonstantinidis/npm-check'>Fork me on <i className='fa fa-github' /></ReactBootstrap.NavItem>
-          </ReactBootstrap.Nav>
-        </ReactBootstrap.CollapsableNav>
-      </ReactBootstrap.Navbar>
+      <Navbar brand='Npm Check' inverse toggleNavKey={0}>
+        <CollapsibleNav eventKey={0}> {/* This is the eventKey referenced */}
+          <Nav navbar right>
+            <NavItem eventKey={1} href='https://github.com/ekonstantinidis/npm-check'>Fork me on <i className='fa fa-github' /></NavItem>
+          </Nav>
+        </CollapsibleNav>
+      </Navbar>
 
     );
   }
