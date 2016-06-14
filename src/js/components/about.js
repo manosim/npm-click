d85b35eb4d7795dc89e46fe9d449989dc4171bae
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-06-14 21:14:32 +0100

    More Fixes

--- src/js/components/about.js
+++ src/js/components/about.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 
 export default class About extends Component {
-
   render() {
     return (
       <div className="container-fluid section-welcome">
@@ -12,22 +11,25 @@ export default class About extends Component {
             <h2>All you need is your package.json. That's all!</h2>
           </div>
         </div>
+
         <div className="row">
-          <div classname="col-md-4">
+          <div className="col-md-4">
             <img src="dist/images/npm-logo.png" className="img-responsive" />
             <p className="lead">
               NPM is awesome! We all use it. Dependencies get updated
               daily so if you want to be up to date, you are at the
               right place.</p>
           </div>
-          <div classname="col-md-4">
+
+          <div className="col-md-4">
             <img src="dist/images/packagejson.png" className="img-responsive" />
             <p className="lead">
               Got your package.json? Drop it on the search bar,
               copy & paste its content to the text box or use the upload.
               Whatever works for you... 'Submit' and that's all!</p>
           </div>
-          <div classname="col-md-4">
+
+          <div className="col-md-4">
             <img src="dist/images/results.png" className="img-responsive" />
             <p className="lead">
               Still not convienced? There's a 'demo' button next to the
