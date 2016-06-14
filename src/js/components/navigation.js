604777603a3e2decce6b5595e11598e2f12a3209
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-06-14 21:18:44 +0100

    More Changes

--- src/js/components/navigation.js
+++ src/js/components/navigation.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
 
-export default class Navigation extends React.Component {
+export default class Navigation extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-inverse">
-        <img
-          className={'navbar-brand img-responsive'}
-          alt="NPM click"
-          src="dist/images/logo.png"
-          onClick={this.openBrowser} />
+        <a href="/">
+          <img
+            className={'navbar-brand img-responsive'}
+            alt="NPM click"
+            src="dist/images/logo.png" />
+        </a>
       </nav>
     );
   }
