bf6ff3eb688d62c55b741353279c17106ceb95d9
Emmanouil Konstantinidis <hello@manos.im>
2020-07-25 15:26:58 +0100

    chore: Setup Layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import * as React from 'react';
 
 export const Navbar = () => {
   return (
-    <nav className="bg-primary">
-      <div className="container flex flex-col lg:flex-row items-center justify-between flex-wrap my-6">
+    <nav className="bg-red-500 py-6">
+      <div className="container flex flex-col lg:flex-row items-center justify-between flex-wrap">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
           <a href="/">
-            <img className="logo" src="/images/logo.png" alt="EM Logo" />
+            <img style={{maxWidth: '8rem'}} src="/logo.svg" alt="NPM.click logo" />
           </a>
         </div>
 
@@ -15,9 +15,10 @@ export const Navbar = () => {
               <li>
                 <a
                   className={`px-2 py-3 lg:inline-block lg:mt-0 lowercase hover:text-blue-400`}
-                  href="/"
+                  href="https://github.com/manosim/npm-click"
+                  rel="noopener noreferrer"
                 >
-                  Test Link
+                  View on GitHub
                 </a>
               </li>
           </ul>
