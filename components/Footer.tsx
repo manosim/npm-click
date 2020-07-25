93b3918ef01bc63ded4135d8f543c378d724209a
Emmanouil Konstantinidis <hello@manos.im>
2020-07-25 15:58:18 +0100

    Add homepage content

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 export const Footer: React.FC = () => {
   return (
-    <div className="bg-gray-800 text-white text-sm py-10 px-3 mt-12">
+    <div className="bg-gray-800 text-white text-sm py-10 px-3">
       <div className="container flex flex-col item-center text-center">
         <div className="my-3">
           Copyright ©{' '}
